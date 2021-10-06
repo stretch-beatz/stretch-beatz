2852cc7ea5fdbf11b00c2877c30f06525c399dcc
Mr Eggleton <meggleton@utcsheffield.org.uk>
2021-10-06 14:16:17 +0100

    Output folder is bach

--- pickleload_test.py
+++ pickleload_test.py
@@ -9,13 +9,19 @@ def main(args):
     with open(pickleFile,'rb') as j:
         data = pickle.load( j)
         print(data)
+        print(len(data["degree"]), "Compasses")
+        
+
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='Process files into the data format for learning.')
-    parser.add_argument('--file', help='file to learn from', default='output/bad_guy.pickle')
+    parser.add_argument('--file', help='file to learn from', default='output/bach.pickle')
     
     args = parser.parse_args()
 
-    main(args)
+
+    print("Scale.minor", Scale.minor)
+
+    #main(args)
