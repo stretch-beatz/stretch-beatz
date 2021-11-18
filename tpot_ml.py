a0aad0fa43a6cb1965043dc29b3c07434ca8ee41
Mr Eggleton <meggleton@utcsheffield.org.uk>
2021-11-18 16:38:06 +0000

    objectified it

--- tpot_ml.py
+++ tpot_ml.py
@@ -11,24 +11,31 @@ import argparse
 
 class MusicLearning:
     dataset = None
-    _data = None
-    _target = None
+    _data = []
+    _target = []
+    _pipeline = None
+    model = ""
+    filename = ""
     inputlength=4
     outputlength=1
 
-    def __init__(self, model, data) -> None:
-        self.model
-        self.dataset
+    def __init__(self, model) -> None:
+        self.model = model
 
+    def set_data(self, data):
+        self.dataset = data
+        self._data = []
+        self._target = []
+        
     @property
     def data(self):
-        if self._data == None :
+        if len(self._data) == 0 :
             self.createDataSet()    
         return self._data
 
     @property
     def target(self):
-        if self._target == None :
+        if len(self._target) == 0 :
             self.createDataSet()    
         return self._target
 
@@ -45,14 +52,40 @@ class MusicLearning:
         
         self._data = np.array(inputdata)
         self._target = np.array(targetdata)
-
+    
     @property
-    def tpotFile(self):
+    def model_file(self):
+        return ospath.join('models',self.model+'.pickle')
+    
+    @property
+    def tpot_file(self):
         return ospath.join("output", self.model + "_tpot.py")
 
     @property
-    def scikitFile(self):
+    def pipeline_file(self):
         return ospath.join("output", self.model + "_scikit.pickle")
+
+    '''
+    @property
+    def data_file(self):
+        return ospath.join("output", self.model + ".pickle")
+    '''
+
+
+    @property
+    def data_json_file(self):
+        return ospath.join("output", self.model + ".json")
+
+    @property
+    def pipeline(self):
+        if self._pipeline == None :
+            if(ospath.isfile(self.pipeline_file)):
+                with open(self.pipeline_file,'rb') as j:
+                    self._pipeline = pickle.load( j)
+            else :
+                self.getTpotModel()
+
+        return self._pipeline
 
     def getTpotModel(self):
         X_train, X_test, y_train, y_test = train_test_split(self.data, self.target,
@@ -63,81 +96,60 @@ class MusicLearning:
         pipeline_optimizer.fit(X_train, y_train)
         print(pipeline_optimizer.score(X_test, y_test))
         
-        pipeline_optimizer.export(self.getTpotFileName())
+        pipeline_optimizer.export(self.tpot_file)
 
-        pickleFile = self.getScikitFileName(self)
+        pickleFile = self.pipeline_file
         print("Saving "+pickleFile)
         with open(pickleFile,'wb') as k:
             pickle.dump(pipeline_optimizer.fitted_pipeline_, k)
 
-        self.pipeline = pipeline_optimizer.fitted_pipeline_
+        self._pipeline = pipeline_optimizer.fitted_pipeline_
 
+    def trainModel(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.data, self.target,
+                                                            train_size=0.75, test_size=0.25)
 
-    def trainModel(digits):
+        self.pipeline.fit(X_train, y_train)
+        results = self.pipeline.predict(X_test)
         
-        getScikitFileName(self)
-
-        exported_pipeline.fit(training_features, training_target)
-        results = exported_pipeline.predict(testing_features)
-        
-        results = exported_pipeline.predict([[7,7,10,7], [7,10,7,5]])
+        #results = self.pipeline.predict([[7,7,10,7], [7,10,7,5]])
         return results
 
-def trainModel(digits):
-    import numpy as np
-    import pandas as pd
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.model_selection import train_test_split
-    from sklearn.naive_bayes import BernoulliNB
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.pipeline import make_pipeline, make_union
-    from tpot.builtins import StackingEstimator
-    from tpot.export_utils import set_param_recursive
 
-    training_features, testing_features, training_target, testing_target = \
-    train_test_split(digits.data, digits.target, random_state=42)
-
-    # Average CV score on the training set was: 0.866000484496124
-    exported_pipeline = make_pipeline(
-        StackingEstimator(estimator=BernoulliNB(alpha=1.0, fit_prior=True)),
-        StackingEstimator(estimator=RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.9000000000000001, min_samples_leaf=5, min_samples_split=6, n_estimators=100)),
-        KNeighborsClassifier(n_neighbors=7, p=1, weights="distance")
-    )
-    # Fix random state for all the steps in exported pipeline
-    set_param_recursive(exported_pipeline.steps, 'random_state', 42)
-
-    exported_pipeline.fit(training_features, training_target)
-    results = exported_pipeline.predict(testing_features)
-    
-    results = exported_pipeline.predict([[7,7,10,7], [7,10,7,5]])
-    return results
+    def expand(self, input, length = 4):
+        output = []+input
+        for i in range(length):
+            prediction = self.pipeline.predict([output[:-self.inputlength]])
+            output += prediction[0]
+        
+        return output
 
 
 def main(args):
     modelName = args.model
-    modelFile = ospath.join('models',modelName+'.pickle')
+
+    ml = MusicLearning(modelName)
+
     pickleFile = args.file
     if(pickleFile == None):
         pickleFile = ospath.join('output',modelName+'.pickle')
     
     print("Loading "+pickleFile)
-    with open(pickleFile,'rb') as j:
-        data = pickle.load( j)
-        #print(data)
-        print(len(data["degree"]), "Compasses")
-        dataset = createDataSet(data['degree'])
-        
-        if args.mode == "tpot":
-            modelfilename = getTpotModel(dataset)
-            print ("modelfilename", modelfilename)
+    if args.mode == "tpot":
+        with open(pickleFile,'rb') as j:
+            data = pickle.load(j)
+            ml.set_data(data['degree'])
+            ml.getTpotModel()
 
-        if args.mode == 'train':
-            modelresults = trainModel(dataset)
-            print(modelresults)
-            
-        if args.mode == 'train':
-            modelresults = trainModel(dataset)
-            print(modelresults)
+    if args.mode == 'train':
+        with open(pickleFile,'rb') as j:
+            data = pickle.load(j)
+            ml.set_data(data['degree'])
+            ml.trainModel()
+        
+    if args.mode == 'test':
+        expanded = ml.expand([7,7,10,5]) 
+        print(expanded)
 
 if __name__ == "__main__":
     
@@ -146,10 +158,5 @@ if __name__ == "__main__":
     parser.add_argument('--file', help='File to load learning data from')
     parser.add_argument('--mode', help='What action to take', choices=['tpot', 'train', 'test'], default="train")
     
-    
     args = parser.parse_args()
-
-
-    #print("Scale.minor", Scale.minor)
-
     main(args)
