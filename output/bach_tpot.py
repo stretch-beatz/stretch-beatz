ce584a15d35cd70478655795213fc288014b2439
Mr Eggleton <meggleton@utcsheffield.org.uk>
2021-11-24 08:15:56 +0000

    better bach model
    trained scikit model is way to big to commit

--- output/bach_tpot.py
+++ output/bach_tpot.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.2361333333333333
-exported_pipeline = XGBClassifier(learning_rate=0.001, max_depth=8, min_child_weight=19, n_estimators=100, n_jobs=1, subsample=0.4, verbosity=0)
+# Average CV score on the training set was: 0.34026666666666666
+exported_pipeline = make_pipeline(
+    SelectPercentile(score_func=f_classif, percentile=79),
+    ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=1.0, min_samples_leaf=4, min_samples_split=4, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
