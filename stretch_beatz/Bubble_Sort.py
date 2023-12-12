4c4b4b87b4abb9232e610ba2ff79765dad84ab60
Mr Eggleton <meggleton@utcsheffield.org.uk>
2023-12-12 13:23:17 +0000

    Update Bubble_Sort.py
    Adds help text

--- stretch_beatz/Bubble_Sort.py
+++ stretch_beatz/Bubble_Sort.py
@@ -8,6 +8,12 @@ Part of his A Level Computer Science Project
 
 @PatternMethod
 def bubble_sort(self, swaps=False, debug = False, palindrome = False): #This defines the section of the code that does the
+    ''' Returns the pattern and each stage of the pattern going through a bubble sort
+        e.g. `P[50,25,5,20,10].bubble_sort()` will return `P[50,25,5,20,10,25,5,20,10,50,5,20,10,25,50,5,10,20,25,50]`
+        and  `P[50,25,5,20,10].bubble_sort(swaps = True)` will add to the output for every number swap returning `P[50,25,5,20,10,25,50,5,20,10,25,5,50,20,10,25,5,20,50,10,25,5,20,10,50,5,25,20,10,50,5,20,25,10,50,5,20,10,25,50,5,10,20,25,50]`
+        and  `P[0.5 ,2.0 ,1 ,1.5].bubble_sort(palindrome=True)` will also undo the change to smoothly return to the orignal pattern P[0.5, 2.0, 1, 1.5, 0.5, 1, 1.5, 2.0, 0.5, 1, 1.5, 2.0, 0.5, 2.0, 1, 1.5]
+    ''''
+
     items = self.data
     changes = passes = 0 # This sets the changes and passes to 0 every time this section is ran.
     last = len(items) #lens is a method that returns the length of a list.
